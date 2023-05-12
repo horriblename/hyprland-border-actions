a5ce7e162d3d90533cc159f2eec5abc24459ab7e
Ching Pei Yang <badnam3o.0@gmail.com>
2023-05-12 14:46:49 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Hyprland plugin to make borders more useful with a mouse.";
+
+  inputs.hyprland.url = "github:hyprwm/Hyprland";
+
+  outputs = {
+    self,
+    hyprland,
+  }: let
+    inherit (hyprland.inputs) nixpkgs;
+    withPkgsFor = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
+  in {
+    packages = withPkgsFor (system: pkgs: {
+      default = pkgs.callPackage ./default.nix {inherit (hyprland.packages.${system}) hyprland;};
+    });
+
+    devShells = withPkgsFor (system: pkgs: {
+      default = pkgs.mkShell {
+        name = "hyprland-plugins";
+        buildInputs = [hyprland.packages.${system}.hyprland];
+        inputsFrom = [hyprland.packages.${system}.hyprland];
+      };
+    });
+
+    formatter = withPkgsFor (system: pkgs: pkgs.alejandra);
+  };
+}
